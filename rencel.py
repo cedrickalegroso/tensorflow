80bd4664383a5951723de2d2f96fb759bf07b456
Cedrick Alegroso <42290274+cedrickalegroso@users.noreply.github.com>
2023-09-22 08:10:52 +0800

    Update rencel.py

--- rencel.py
+++ rencel.py
@@ -18,64 +18,66 @@ export_dir = os.path.abspath(export_dir)
 
 
 
-image_path = tf.keras.utils.get_file(
-      'thesis_dataset.tgz',
-      'https://2021.filemail.com/api/file/get?filekey=1QyoR9kv-1drJb-Lok0RXcjE0uH4MRLAQhL5YzvDwiNq4g-NpVBiyUIoasaSAImVLK45eQ',
-      extract=True)
+#image_path = tf.keras.utils.get_file(
+#      'thesis_dataset.tgz',
+#      'https://2021.filemail.com/api/file/get?filekey=1QyoR9kv-1drJb-Lok0RXcjE0uH4MRLAQhL5YzvDwiNq4g-NpVBiyUIoasaSAImVLK45eQ',
+#      extract=True)
 image_path = os.path.join(os.path.dirname(image_path), 'thesis_dataset')
 
-data = DataLoader.from_folder(image_path)
+print(image_path)
 
-train_data, rest_data = data.split(0.8)
-validation_data, test_data = rest_data.split(0.5)
+# data = DataLoader.from_folder(image_path)
 
-plt.figure(figsize=(10,10))
-for i, (image, label) in enumerate(data.gen_dataset().unbatch().take(25)):
-  plt.subplot(5,5,i+1)
-  plt.xticks([])
-  plt.yticks([])
-  plt.grid(False)
-  plt.imshow(image.numpy(), cmap=plt.cm.gray)
-  plt.xlabel(data.index_to_label[label.numpy()])
-plt.show()
+# train_data, rest_data = data.split(0.8)
+# validation_data, test_data = rest_data.split(0.5)
 
-model = image_classifier.create(train_data, validation_data=validation_data)
+# plt.figure(figsize=(10,10))
+# for i, (image, label) in enumerate(data.gen_dataset().unbatch().take(25)):
+#   plt.subplot(5,5,i+1)
+#   plt.xticks([])
+#   plt.yticks([])
+#   plt.grid(False)
+#   plt.imshow(image.numpy(), cmap=plt.cm.gray)
+#   plt.xlabel(data.index_to_label[label.numpy()])
+# plt.show()
 
-model.summary()
+# model = image_classifier.create(train_data, validation_data=validation_data)
 
-loss, accuracy = model.evaluate(test_data)
+# model.summary()
 
-# A helper function that returns 'red'/'black' depending on if its two input
-# parameter matches or not.
-def get_label_color(val1, val2):
-  if val1 == val2:
-    return 'black'
-  else:
-    return 'red'
+# loss, accuracy = model.evaluate(test_data)
 
-# Then plot 100 test images and their predicted labels.
-# If a prediction result is different from the label provided label in "test"
-# dataset, we will highlight it in red color.
-plt.figure(figsize=(20, 20))
-predicts = model.predict_top_k(test_data)
-for i, (image, label) in enumerate(test_data.gen_dataset().unbatch().take(100)):
-  ax = plt.subplot(10, 10, i+1)
-  plt.xticks([])
-  plt.yticks([])
-  plt.grid(False)
-  plt.imshow(image.numpy(), cmap=plt.cm.gray)
+# # A helper function that returns 'red'/'black' depending on if its two input
+# # parameter matches or not.
+# def get_label_color(val1, val2):
+#   if val1 == val2:
+#     return 'black'
+#   else:
+#     return 'red'
 
-  predict_label = predicts[i][0][0]
-  color = get_label_color(predict_label,
-                          test_data.index_to_label[label.numpy()])
-  ax.xaxis.label.set_color(color)
-  plt.xlabel('Predicted: %s' % predict_label)
-plt.show()
+# # Then plot 100 test images and their predicted labels.
+# # If a prediction result is different from the label provided label in "test"
+# # dataset, we will highlight it in red color.
+# plt.figure(figsize=(20, 20))
+# predicts = model.predict_top_k(test_data)
+# for i, (image, label) in enumerate(test_data.gen_dataset().unbatch().take(100)):
+#   ax = plt.subplot(10, 10, i+1)
+#   plt.xticks([])
+#   plt.yticks([])
+#   plt.grid(False)
+#   plt.imshow(image.numpy(), cmap=plt.cm.gray)
 
-print(export_dir)
+#   predict_label = predicts[i][0][0]
+#   color = get_label_color(predict_label,
+#                           test_data.index_to_label[label.numpy()])
+#   ax.xaxis.label.set_color(color)
+#   plt.xlabel('Predicted: %s' % predict_label)
+# plt.show()
 
-model.export(export_dir=export_dir, export_format=ExportFormat.TFLITE)
+# print(export_dir)
 
-#model.export(export_dir='/home/rencel/', export_format=ExportFormat.LABEL)
+# model.export(export_dir=export_dir, export_format=ExportFormat.TFLITE)
 
-#model.evaluate_tflite('/home/rencel/model.tflite', test_data) 
+# #model.export(export_dir='/home/rencel/', export_format=ExportFormat.LABEL)
+
+# #model.evaluate_tflite('/home/rencel/model.tflite', test_data) 
